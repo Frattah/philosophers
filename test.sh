ab2f240f9073f1675a7b7a762779b299a3eb5ff2
Francesco Monfrecola <frmonfre@e3r10p2.42roma.it>
2023-05-08 11:34:49 +0200

    test aggiornato

--- test.sh
+++ test.sh
@@ -15,23 +15,27 @@ echo "\n\nTest #3 (10 190 200 0)"
 echo "\n\nTest #4 (1 800 200 200 1)"
 ./philo 1 800 200 200 1 | grep "died"
 
-echo "\n\nTest #5 (5 800 200 200 10)"
-./philo 5 800 200 200 10 | grep "is eating" | wc -l
+echo "\n\nTest #5 (5 800 200 200 5)"
+./philo 5 800 200 200 5 | grep "is eating" | wc -l
+./philo 5 800 200 200 5 | grep "died"
 
 echo "\n\nTest #6 (5 300 200 120 1)"
 ./philo 5 300 200 120 1 | grep "died"
 
-echo "\n\nTest #7 (4 410 200 200 10)"
-./philo 4 410 200 200 10 | grep "is eating" | wc -l
+echo "\n\nTest #7 (4 410 200 200 5)"
+./philo 4 410 200 200 5 | grep "is eating" | wc -l
+./philo 4 800 200 200 5 | grep "died"
 
 echo "\n\nTest #8 (4 310 200 100 10)"
 ./philo 4 310 200 100 10 | grep "died"
 
 echo "\n\nTest #9 (20 800 200 200 1)"
 ./philo 20 800 200 200 1 | grep "is eating" | wc -l
+./philo 20 800 200 200 1 | grep "died"
 
 echo "\n\nTest #10 (50 800 200 200 1)"
 ./philo 50 800 200 200 1 | grep "is eating" | wc -l 
+./philo 50 800 200 200 1 | grep "died"
 
 echo "\n\nTest #11 (3 410 200 200)"
 ./philo 3 410 200 200 | grep "died"
